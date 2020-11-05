256af3f70918e5937948511e7365e3799b759729
Pamphile <pamphile.saltel@wanadoo.fr>
2020-11-05 13:05:57 +0100

    added progression checker script and prefab example

---
+++ Cooperatif Mario/Assets/Scripts/ProgressionChecker.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressionChecker : MonoBehaviour
+{
+    public Transform beginning;
+    public Transform end;
+    public Transform player;
+
+    public float totalHorizontalDistance;
+    public float playerDistance;
+    public float percentageDone = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // test no null
+        if (!player) // if player is not set yet we search for the first gameObject with the tag "Player" instead
+        {
+            player = GameObject.FindWithTag("Player").GetComponent<Transform>();
+        }
+
+        totalHorizontalDistance = end.position.x - beginning.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerDistance = player.position.x - beginning.position.x;
+
+        percentageDone = (playerDistance / totalHorizontalDistance) * 100;
+    }
+}
