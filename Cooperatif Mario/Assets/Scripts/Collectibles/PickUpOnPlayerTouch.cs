d689fe208867a06f831442c8c0cbe635fd733634
Pamphile <pamphile.saltel@wanadoo.fr>
2020-11-05 01:33:21 +0100

    First implementation of collectibles (Coin and PowerUp) (may need to rework/clean Unity layers)

---
+++ Cooperatif Mario/Assets/Scripts/Collectibles/PickUpOnPlayerTouch.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpOnPlayerTouch : MonoBehaviour
+{
+    public enum CollectibleType { Ore, PowerUp };
+
+    public CollectibleType type = CollectibleType.Ore;
+    public PlayerState player;
+
+    public GameObject parentToDestroy;
+    public GameObject[] toDirectDestroy;
+    public GameObject[] toSemiDirectDestroy;
+
+    private void Start()
+    {
+    // verify that playerstate isn't null;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            switch(type)
+            {
+                case CollectibleType.Ore:
+                    player.orePickedUp++;
+                    break;
+
+                case CollectibleType.PowerUp:
+                    player.IsPowerUpped = true;
+                    break;
+
+                default:
+                    break;
+            }
+
+            foreach (GameObject go in toDirectDestroy)
+            {
+                Destroy(go);
+            }
+
+            foreach (GameObject go in toSemiDirectDestroy)
+            {
+                Destroy(go, 0.1f);
+            }
+
+            // launch pickup animation;
+
+            Destroy(parentToDestroy, 1f);
+
+            Destroy(this.gameObject);
+
+        }
+    }
+}
---
+++ Cooperatif Mario/Assets/Scripts/Player/PlayerState.cs
@@ -0,0 +1,23 @@
+﻿// A script to manage the number of Ore collected and the actual state of the player (if they have a powerUp or not)
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState : MonoBehaviour
+{
+    public bool IsPowerUpped = false;
+    public int orePickedUp = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
