a96c2be61fbf015aa9368957b64a6d3a48e2a437
Matthieu <matthieu.gb.93@gmail.com>
2020-11-05 09:54:55 +0100

    ReactionToTrigger

---
+++ Cooperatif Mario/Assets/Scripts/Traps/ReactionOnTrigger.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReactionOnTrigger : MonoBehaviour
+{
+    public GameObject m_gameobject;
+    public SpriteRenderer Sprite;
+    public bool willAppear, willDisAppear, goToTarget;
+    public bool willReAppear, willReDisAppear;
+    public float speed = 0;
+    public Transform target;
+
+    private Vector3 posVec;
+    private bool move;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        posVec = target.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (move)
+        {
+            m_gameobject.transform.position = Vector3.MoveTowards(transform.position, posVec, speed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (goToTarget) move = true;
+            if (willAppear) Sprite.enabled = true;
+            if (willDisAppear) Sprite.enabled = false;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (willReAppear) Sprite.enabled = true;
+            if (willReDisAppear) Sprite.enabled = false;
+        }
+    }
+}
