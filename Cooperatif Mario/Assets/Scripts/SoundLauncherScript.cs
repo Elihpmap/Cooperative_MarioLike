58c1463ef87835c8152d747e12570dd1a6e841c3
Pamphile <pamphile.saltel@wanadoo.fr>
2020-11-06 10:27:13 +0100

    added simple soundLauncher with test values

---
+++ Cooperatif Mario/Assets/Scripts/SoundLauncherScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundLauncherScript : MonoBehaviour
+{
+    public enum SoundEvent {test1 , test2};
+    public string[] stringValue = { "Play_Tableau3_Part1", "Play_Tableau3_Part1" };
+
+    public SoundEvent soundToPlay = SoundEvent.test1;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        AkSoundEngine.PostEvent(stringValue[(int)soundToPlay], gameObject);
+    }
+}
