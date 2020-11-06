bea82cfbfbe9b0e5f7bf0a64214fa0cce8c6f4c9
Matthieu <matthieu.gb.93@gmail.com>
2020-11-06 14:38:12 +0100

    correction bug

--- Cooperatif Mario/Assets/Scripts/Traps/Spike.cs
+++ Cooperatif Mario/Assets/Scripts/Traps/Spike.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class Spike : MonoBehaviour
 {
-
+    public bool onShoot = false;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player" && !GameOverGestion.alreadyDead)
         {
+            if (onShoot) collision.gameObject.GetComponent<PlayerState>().IsPowerUpped = false;
             GameObject GO = GameObject.Find("GameOver");
             GO.GetComponent<GameOverGestion>().StartCoroutine("GameOverStart", 1f);
 
